637c75cee6ea5f4a51189d704c20643360403ea9
Iker Sierra Plaza <iker.siepla@educa.jcyl.es>
2022-05-10 11:08:11 +0200

    Vuelta a la normalidad

--- src/jcolonia/daw2021/HolaMundo.java
+++ src/jcolonia/daw2021/HolaMundo.java
@@ -8,7 +8,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
-		System.out.println("HolaMundo");
+		System.out.println("HolaMundo!");
 
 	}
 
