df776cb5b18a797fbcb0ca7010b3d8e0d5275b30
Iker Sierra Plaza <iker.siepla@educa.jcyl.es>
2022-05-10 10:39:13 +0200

    ASDASD

--- src/jcolonia/daw2021/HolaMundo.java
+++ src/jcolonia/daw2021/HolaMundo.java
@@ -8,7 +8,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
-		System.out.println("Hola soy gei");
+		System.out.println("Hola soy gei!");
 
 	}
 
