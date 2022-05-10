a7a7018dd755d0300a829b0c5766bdc5de0f23da
Iker Sierra Plaza <iker.siepla@educa.jcyl.es>
2022-05-10 10:01:41 +0200

    Nueva clase

---
+++ src/jcolonia/daw2021/HolaMundo.java
@@ -0,0 +1,14 @@
+package jcolonia.daw2021;
+
+public class HolaMundo {
+
+	public HolaMundo() {
+		// TODO Esbozo de constructor generado automáticamente
+	}
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
